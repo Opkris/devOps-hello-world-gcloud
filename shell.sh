e500a803599750b6a7bc30bcad613f8f11c39562
OpKris <kristofferopdahl@mail.com>
2020-10-05 10:11:37 +0200

    fixed shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag pgr301 --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
-docker tag pgr301 opkris/fanatastichelloapp
-docker push opkris/fantastichelloapp:pgr301
+docker build . --tag fantastichelloapp --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
+docker tag fantastichelloapp opkris/fantastichelloapp
+docker push opkris/fantastichelloapp
