7ce6d5ddd4516d5789702021205425897b0fbb00
OpKris <kristofferopdahl@mail.com>
2020-10-05 09:43:07 +0200

    done some changes to demoApplication

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag helloworld --build-arg JAR_FILE=./target/devOps-hello-world-0.0.1-SNAPSHOT.jar
+docker build . --tag helloworld --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
 docker tag helloworld  opkris/fanatastichelloapp
 docker push opkris/fanatastichelloapp
