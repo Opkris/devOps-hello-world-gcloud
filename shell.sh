3a8d288cc6006342bfcb70387d308e914d745bf8
OpKris <kristofferopdahl@mail.com>
2020-10-05 13:04:52 +0200

    done some name changes, and created a jar file

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag helloworldgcloud --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
+docker build . --tag helloworldgcloud --build-arg JAR_FILE=./target/devOps-hello-world-gcloud-1.0-SNAPSHOT.jar
 docker tag helloworldgcloud opkris/helloworldgcloud
 docker push opkris/helloworldgcloud
