88938fe83a031f56810d392eadfdcf1e7c0810cd
OpKris <kristofferopdahl@mail.com>
2020-10-05 12:41:14 +0200

    fixed shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag fantastichelloapp --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
-docker tag fantastichelloapp opkris/fantastichelloapp
-docker push opkris/fantastichelloapp
+docker build . --tag helloworldgcloud --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
+docker tag helloworldgcloud opkris/helloworldgcloud
+docker push opkris/helloworldgcloud
