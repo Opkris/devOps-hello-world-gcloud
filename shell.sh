0feae2f98fa1209e3b111645ca64f8679fd4bc6b
OpKris <kristofferopdahl@mail.com>
2020-10-05 10:02:46 +0200

    fixed travis.yml

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag helloworld --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
-docker tag helloworld  opkris/fanatastichelloapp
+docker build . --tag pgr301 --build-arg JAR_FILE=./target/devOps-hello-world-1.0-SNAPSHOT.jar
+docker tag pgr301 opkris/fanatastichelloapp
 docker push opkris/fanatastichelloapp
